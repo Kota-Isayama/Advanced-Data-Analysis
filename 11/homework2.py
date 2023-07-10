114134fb9c9e210864aa109ab7afaf810b928f57
Kota-Isayama <isakou.2208@icloud.com>
2023-07-10 23:42:33 +0900

    Add class11

---
+++ 11/homework2.py
@@ -0,0 +1,82 @@
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+import math
+np.random.seed(1)
+
+def data_generation1(n=100):
+    return np.concatenate([np.random.randn(n, 1) * 2, np.random.randn(n, 1)], axis=1)
+
+def data_generation2(n=100):
+    return np.concatenate([np.random.randn(n, 1) * 2, 2 * np.round(np.random.rand(n, 1)) - 1 + np.random.randn(n, 1) / 3.], axis=1)
+
+def data_generation3(n=100):
+    x = np.concatenate([np.linspace(-2, 2, n).reshape(n, -1), np.ones((n, 1)) * (1 - 2 * (np.arange(n) % 2)).reshape(n, -1)], axis=1)
+    # return np.where(np.arange(x.shape[0]) % 2, -x, x)
+    print(x)
+    return x
+
+def similarity_matrix(x, t=1.):
+    W = np.exp(-np.linalg.norm(x[None] - x[:,None,:], ord=2,axis=2) / (2*t*t))
+    return W
+
+def similarity_matrix2(x, t=1.):
+    W = np.zeros((x.shape[0], x.shape[0]))
+    for i in range(x.shape[0]):
+        for j in range(x.shape[0]):
+            W[i][j] = np.exp(-np.sum((x[i] - x[j])**2) / (t * t))
+    return W
+
+def similarity_matrix3(x, k=5):
+    d = np.linalg.norm(x[None] - x[:, None, :], ord=2, axis=2)
+    indices = np.argsort(d, axis=1)[:,:k]
+    W = np.zeros((x.shape[0], x.shape[0]))
+    for i in range(x.shape[0]):
+        W[i, indices[i]] = 1
+        W[indices[i], i] = 1
+    return W
+
+
+def lpp(x, n_components=1):
+    x -= np.mean(x, axis=0)
+    # W = similarity_matrix(x)
+    W = similarity_matrix2(x)
+    d = np.sum(W, axis=1)
+    D = np.diag(d)
+    L = D - W
+    A = x.T.dot(L).dot(x)
+    B = x.T.dot(D).dot(x)
+    # rB = sp.linalg.sqrtm(B)
+    # irB = np.linalg.inv(rB)
+    # C = irB.dot(A).dot(irB)
+    # w, v = sp.linalg.eigh(C)
+    w, v = sp.linalg.eigh(A, B)
+    print(w)
+    print("labmda")
+    print(A.dot(v) / B.dot(v))
+    print(v)
+    print(np.linalg.norm(v, 2, 0, keepdims=True))
+    v = v / np.linalg.norm(v, 2, 0, keepdims=True)
+    print("v")
+    print(v)
+    print(np.linalg.norm(v, 2, 0))
+    return w[:n_components], v[:,:n_components]
+
+n = 100
+n_components = 1
+# x = data_generation3(n)
+x = data_generation2(n)
+# print(x.shape)
+
+w, v = lpp(x, n_components)
+
+# lpp = LPP(n_components)
+# v = lpp.fit(x)
+
+plt.xlim(-6., 6.)
+plt.ylim(-6., 6.)
+plt.plot(x[:, 0], x[:, 1], 'rx')
+# plt.show()
+# plt.plot(np.array([-v[:,0], v[:,0]]) * 9, np.array([-v[:,1], v[:,1]]) * 9)
+plt.plot(np.array([-v[0], v[0]]) * 9, np.array([-v[1], v[1]]) * 9)
+plt.savefig("11/lpp_homework21.png")
