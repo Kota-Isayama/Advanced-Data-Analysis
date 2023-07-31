c4504310b9786560fac1a98ace3c78331340781f
Kota-Isayama <isakou.2208@icloud.com>
2023-07-31 16:08:00 +0900

    Add homework12

---
+++ 12/problem1.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib
+from scipy import linalg
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+np.random.seed(10)
+
+def data_generation1(n):
+    x = np.random.randn(n, 2)
+    x[0:n//2, 0] = x[0:n//2, 0] - 4
+    x[n//2:, 0] = x[n//2:, 0] + 4
+    x = x - np.mean(x, axis=0, keepdims=True)
+    y = np.concatenate([np.zeros(n//2), np.ones(n - n//2)])
+    return x, y
+
+def data_generation2(n):
+    x = np.random.randn(n, 2)
+    x[0:n//4, 0] = x[0:n//4, 0] - 4
+    x[n//4:n//2, 0] = x[n//4:n//2, 0] + 4
+    x = x - np.mean(x, axis=0, keepdims=True)
+    y = np.concatenate([np.zeros(n//2), np.ones(n - n//2)])
+    return x, y
+
+def fda(x, y,  n_class, n_components=1):
+    d = x.shape[1]
+    Sb = np.zeros((d, d))
+    for c in range(n_class):
+        mu = np.mean(x[y==c], axis=0)
+        n = np.sum(y==c)
+        Sb += n * mu.dot(mu.T)
+    C = np.zeros((d, d))
+    for i in range(x.shape[0]):
+        C += np.matmul(x[i].reshape(d, -1), x[i].reshape(-1, d))
+
+    Sw = C - Sb
+
+    w, v = linalg.eig(Sb, Sw)
+    return w[:n_components], v[:, :n_components]
+
+n = 100
+n_components = 1
+x, y = data_generation1(n)
+# x, y = data_generation2(n)
+w, v = fda(x, y, 2, n_components=n_components)
+plt.xlim(-6., 6.)
+plt.ylim(-6., 6.)
+plt.plot(x[y==0, 0], x[y==0, 1], 'bo')
+plt.plot(x[y==1, 0], x[y==1, 1], 'rx')
+plt.plot(np.array([-v[0], v[0]]) * 9, np.array([-v[1], v[1]]) * 9)
+plt.show()
---
+++ 12/problem4.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib
+from scipy import linalg
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+np.random.seed(1)
+
+def generate_data(sample_size, n_class):
+    x = (np.random.normal(size=(sample_size//n_class, n_class)) + np.linspace(-3., 3., n_class)).flatten()
+    y = np.broadcast_to(np.arange(n_class), (sample_size//n_class, n_class)).flatten()
+    return x, y
+
+def least_squares_prob_clr(x, y, h, l, n_class):
+    sample_size = len(x)
+    # theta = np.zeros((sample_size, n_class))
+
+    Phi = np.exp(- (x[:, None] - x[None])**2 / (2 * h*h))
+    Pi = np.zeros((sample_size, n_class))
+    for i in range(sample_size):
+        for c in range(n_class):
+            if c == y[i]:
+                Pi[i][c] = 1
+
+    # print(y)
+    # print("Pi", Pi)
+
+    theta = np.linalg.solve(
+        Phi.T.dot(Phi) + l * np.eye(sample_size),
+        Phi.T.dot(Pi)
+    )
+
+    return theta
+
+
+def visualize(x, y, theta, h):
+    X = np.linspace(-5., 5., num=100)
+    K = np.exp(-(x - X[:, None])**2 / (2 * h*h))
+    plt.clf()
+    plt.xlim(-5, 5)
+    plt.ylim(-.3, 1.8)
+    prob = K.dot(theta)
+    prob = np.where(prob > 0, prob, 0)
+    prob = prob / np.sum(prob, axis=1, keepdims=True)
+    # print(prob)
+    # unnormalized_prob = np.exp(logit - np.max(logit, axis=1, keepdims=True))
+    # prob = unnormalized_prob / unnormalized_prob.sum(1, keepdims=True)
+    plt.plot(X, prob[:, 0], c='blue')
+    plt.plot(X, prob[:, 1], c='red')
+    plt.plot(X, prob[:, 2], c='green')
+    plt.scatter(x[y == 0], -.1 * np.ones(len(x) // 3), c='blue', marker='o')
+    plt.scatter(x[y == 1], -.2 * np.ones(len(x) // 3), c='red', marker='x')
+    plt.scatter(x[y == 2], -.1 * np.ones(len(x) // 3), c='green', marker='v')
+    plt.savefig('11/fig.png')
+
+x, y = generate_data(sample_size=90, n_class=3)
+theta = least_squares_prob_clr(x, y, h=2., l=.1, n_class=3)
+visualize(x, y, theta, h=2.)
